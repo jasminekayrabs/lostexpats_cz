1aa6d539cc1952212d78398907414bb0e4427224
jasminekayrabs <80991158+jasminekayrabs@users.noreply.github.com>
2023-05-08 18:37:26 +0200

    Update views.py successful signup & redirect to login
    File was updated to print a successful signup message, create user and redirect user to login page

--- lostexpats_cz/authentication/views.py
+++ lostexpats_cz/authentication/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages 
 
 # Create your views here.
 #Taking user input on the back-end and saving it to the database
@@ -6,6 +9,23 @@ def home(request):
     return render(request, "authentication/index.html")
 
 def render_signup(request):
+    if request.method == "POST":
+        firstname = request.POST['first_name']
+        lastname = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['pass1']
+        confpassword = request.POST['pass2']
+#to create a user
+        myuser = User.objects.create_user(firstname, email,password)
+        myuser.first_name = firstname
+        myuser.last_name = lastname 
+
+        myuser.save()
+#printing succeful signup message
+        messages.success(request, "Your account has been created.")
+
+#redirecting user to login
+        return redirect('render_login')
     return render(request, "authentication/signup.html")
 
 def render_login(request):
