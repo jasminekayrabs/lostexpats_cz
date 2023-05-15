f21bf074ede59da2d19fda31d747899359540682
Sara mojadami <97891856+saramoj@users.noreply.github.com>
2023-05-15 19:31:27 +0200

    this file will allow u to make different fields in the database filemodels.py
    in order to make it work follow these steps 
    add the following code to admins.py :
    from django.contrib import admin
    from .models import MyModel
    
    admin.site.register(MyModel)
    ####( I will commit this next)
    
    on your terminal type:
    python manage.py runserver
    python manage.py shell 
    #this command will lead you to the shell where you will type the following : 
    
    from authentication.models import MyModel
    my_model_instances = MyModel.objects.all()
    
    for instance in my_model_instances:
        print(instance.fname, instance.email)
    
    
    (this only works if you're an admin) so make yourself an admin if you are not

--- lostexpats_cz/authentication/models.py
+++ lostexpats_cz/authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class MyModel(models.Model):
+    fname = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
