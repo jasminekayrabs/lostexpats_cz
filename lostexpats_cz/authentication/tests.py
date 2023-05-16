4d0bd894c06b8a908d4046bd02e05f01d665ac56
jasminekayrabs <80991158+jasminekayrabs@users.noreply.github.com>
2023-05-17 01:00:41 +0200

    add CSRF test case to tests.py

--- lostexpats_cz/authentication/tests.py
+++ lostexpats_cz/authentication/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.middleware.csrf import get_token
+
+
 
 #To run tests, run command "python manage.py test" on terminal
 #Test case for XSS protection
@@ -26,3 +30,47 @@ class XSSProtectionTestCase(TestCase):
 
         # Ensure that the malicious input is properly escaped in the rendered HTML
         self.assertNotContains(response, malicious_input)
+
+        
+        
+#TEST FOR CSRF PROTECTION
+class CSRFProtectionTestCase(TestCase):
+    def setUp(self):
+        self.signup_url = reverse('render_signup')
+        self.login_url = reverse('render_login')
+
+    def test_signup_csrf_protection(self):
+        # Create a user for authentication
+        user = User.objects.create_user(username='testuser', password='testpassword')
+
+        response = self.client.get(self.signup_url)
+        self.assertEqual(response.status_code, 200)
+        
+        # Use force_login to authenticate the user
+        self.client.force_login(user)
+
+        # Submit the signup form
+        response = self.client.post(
+            self.signup_url,
+            {'fname': 'John', 'lname': 'Doe', 'email': 'john@example.com', 'pass1': 'password', 'pass2': 'password'},
+            follow=True
+        )
+        self.assertEqual(response.status_code, 200)  # Successful signup
+
+    def test_login_csrf_protection(self):
+        # Create a user for authentication
+        user = User.objects.create_user(username='testuser', password='testpassword')
+
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        
+        # Use force_login to authenticate the user
+        self.client.force_login(user)
+
+        # Submit the login form
+        response = self.client.post(
+            self.login_url,
+            {'email': 'john@example.com', 'pass1': 'password'},
+            follow=True
+        )
+        self.assertEqual(response.status_code, 200)  # Successful login
