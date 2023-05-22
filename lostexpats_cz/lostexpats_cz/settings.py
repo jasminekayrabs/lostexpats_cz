ae88d66bd34b0993597c82d0721e4168555d1cbe
Sara mojadami <97891856+saramoj@users.noreply.github.com>
2023-05-22 15:52:34 +0200

    comments on settings.py for clickjacking measures

--- lostexpats_cz/lostexpats_cz/settings.py
+++ lostexpats_cz/lostexpats_cz/settings.py
@@ -60,7 +60,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
+#When protecting the webiste from click-jacking there are 
+#three settings to choose from which include 'same-origin', 'deny', and 'ALLOW-FROM origin'(the last option does not work on most 
+# browsers now). For our application we chose to 'deny' because it denies all options of framing which gives the least risk for clickjacking too. 
 X_FRAME_OPTIONS = 'DENY'
  #The page cannot be displayed in a frame, regardless of the site attempting to do so.
 
