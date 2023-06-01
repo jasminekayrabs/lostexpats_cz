7610b58855b7a9b731fd94ddfe6d59005416ff75
Sara mojadami <97891856+saramoj@users.noreply.github.com>
2023-06-01 21:11:49 +0200

    Saras tests commented  tests.py

--- lostexpats_cz/authentication/tests.py
+++ lostexpats_cz/authentication/tests.py
@@ -169,13 +169,21 @@ class AuthenticationTestCase(TestCase):
         self.assertEqual(response.status_code, 301)
        
     
+# This tests checks to see 1. Existence of the log file:
+# 2. Configuration of the logger: It configures the logger named 
+# 'user_login' to log messages with the INFO level
+# 3.Logging of a test message: It logs the message 'This is a test log message' 
+# using the configured logger.
+# 4.Presence of the test message in the log file: It reads the contents of the log file specified by log_file_path and checks if the test message ('This is a test log message') is 
+# present in the log file using the assertIn assertion.
+# Done by Sara 
 class LoggingTest(TestCase):
     def test_logging_file(self):
         # Define the log file path
-        log_file_path = os.path.join(settings.BASE_DIR, 'logs', 'user_login.log')
+        log_file_path = os.path.join(settings.BASE_DIR, 'logs', '/Users/saraali/Desktop/python_stuff/lostexpats_cz/logs/user_login.log')
 
         # Ensure the log file exists
-        self.assertTrue(os.path.exists('logs/user_login.log'))
+        self.assertTrue(os.path.exists('/Users/saraali/Desktop/python_stuff/lostexpats_cz/logs/user_login.log'))
 
         # Configure the logger
         logger = logging.getLogger('user_login')
@@ -185,11 +193,16 @@ class LoggingTest(TestCase):
         logger.info('This is a test log message')
 
         # Read the log file and check if the test message is present
-        with open(log_file_path, 'r') as file:
+        with open('/Users/saraali/Desktop/python_stuff/lostexpats_cz/logs/user_login.log', 'r') as file:
             log_contents = file.read()
             self.assertIn('This is a test log message', log_contents)
+
             
-            
+# This test checks to see if the is session is secure 
+# The purpose of the secure attribute is to prevent cookies
+# from being observed by unauthorized parties 
+# due to the transmission of the cookie in clear text.
+# Done by Sara
 class SessionManagementTest(TestCase):
     def test_secure_session(self):
     # Create a client and make a request to a secured view or URL
@@ -209,14 +222,36 @@ class SessionManagementTest(TestCase):
            self.assertTrue(sessionid_cookie.get('httponly'))
             
             
+            
+# HSTS test
+# 1. It sends a GET request to the 'home' URL with the secure=True parameter, simulating an HTTPS request.
+#  This ensures that the request is made over a secure connection.
+# 2. It checks if the response status code is 200 (OK) using the assertEqual assertion. 
+# This confirms that the URL is accessible and the server responds successfully.
+# 3. t checks if the 'Strict-Transport-Security' header is present in the response
+# using the assertIn assertion
+# 4. It retrieves the value of the 'Strict-Transport-Security' header from the response.
+# 5.It checks if the value of the 'Strict-Transport-Security' header matches the expected value using the assertEqual assertion.
+# In this case, the expected value is 'max-age=31536000; includeSubDomains', which 
+# specifies the HSTS policy's maximum age and includes all subdomains.
+# Done by Sara
 class HSTSTestCase(TestCase):
     def test_hsts_redirection(self):
+        # Send a GET request to the 'home' URL with secure=True to simulate an HTTPS request
         response = self.client.get(reverse('home'), secure=True)
+
+        # Check if the response status code is 200 (OK)
         self.assertEqual(response.status_code, 200)
+
+        # Check if the 'Strict-Transport-Security' header is present in the response
         self.assertIn('Strict-Transport-Security', response.headers)
+
+        # Retrieve the value of the 'Strict-Transport-Security' header from the response
         hsts_header = response.headers['Strict-Transport-Security']
+
+        # Check if the value of the 'Strict-Transport-Security' header matches the expected value
         self.assertEqual(hsts_header, 'max-age=31536000; includeSubDomains')
-    
+
     
   
 # X frame options 
